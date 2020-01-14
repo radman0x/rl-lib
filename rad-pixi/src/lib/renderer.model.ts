6c36f1063ecd467aeafb8497c80c09600dc7361b
Conrad Mercer <conrad.mercer@gmail.com>
2020-01-15 07:50:52 +1030

    RAD-473: WIP improve renderer libs

--- rad-pixi/src/lib/renderer.model.ts
+++ rad-pixi/src/lib/renderer.model.ts
@@ -1,9 +1,6 @@
 import * as PIXI from 'pixi.js-legacy';
-import { Entity, EntityManager } from 'rad-ecs';
-import { Observable, Subject } from 'rxjs';
-import { distinctUntilChanged, take } from 'rxjs/operators';
-import { GridPos } from 'libs/rl-ecs/src/lib/components/position.model';
-import { Renderable } from 'libs/rl-ecs/src/lib/components/renderable.model';
+import { Observable, ReplaySubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 export interface RendererSettings {
   tileSize: number;
@@ -11,149 +8,66 @@ export interface RendererSettings {
 }
 
 export class Renderer {
-  get tileSize() {
-    return this.settings.tileSize;
-  }
+  public pixiApp: PIXI.Application;
+  private resources: PIXI.LoaderResource;
 
-  private constructor(
-    private em: EntityManager,
-    private settings: RendererSettings,
-    options: object
-  ) {
+  private constructor(options: object) {
     PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
     this.pixiApp = new PIXI.Application(options);
-    this.desiredDisplayWidthPx =
-      settings.tileSize * settings.displayWidthInTiles;
-
-    this.pixiApp.renderer.plugins.interaction.on('pointermove', event => {
-      if (this.pixiApp.stage) {
-        this.mouseOverGridPos.next(
-          this.convertToGridPos(event.data.getLocalPosition(this.pixiApp.stage))
-        );
-      }
-    });
-
-    this.pixiApp.renderer.plugins.interaction.on('pointerdown', event => {
-      if (this.pixiApp.stage) {
-        this.mousePressGridPos.next(
-          this.convertToGridPos(event.data.getLocalPosition(this.pixiApp.stage))
-        );
-      }
-    });
   }
 
-  get view() {
-    return this.pixiApp.view;
-  }
-
-  get mouseOver$() {
-    return this.mouseOverGridPos.pipe(
-      distinctUntilChanged((lhs, rhs) => {
-        if (lhs === rhs) {
-          return true;
-        } else if (!lhs || !rhs) {
-          return false;
-        } else {
-          return lhs.x === rhs.x && lhs.y === rhs.y;
+  static create(sheet: string, pixiAppOptions: object): Observable<Renderer> {
+    const subject = new ReplaySubject<Renderer>();
+    const renderer = new Renderer(pixiAppOptions);
+    renderer.pixiApp.loader.add(sheet).load((loader, resources) => {
+      const errors: string[] = [];
+      for (const res of Object.values(resources)) {
+        if (res.error) {
+          errors.push(`Failed to load: ${res.url}, because: ${res.error}`);
         }
-      })
-    );
-  }
-
-  get mousePress$() {
-    return this.mousePressGridPos;
-  }
-
-  public pixiApp: PIXI.Application;
-  private sprites = new Map<number, PIXI.Sprite>();
-  private resources: PIXI.LoaderResource;
-  private desiredDisplayWidthPx: number;
-
-  public maxTileY: number;
-  public maxTileX: number;
-
-  private mouseOverGridPos = new Subject<{ x: number; y: number } | null>();
-  private mousePressGridPos = new Subject<{ x: number; y: number } | null>();
-  static create(
-    em: EntityManager,
-    sheet: string,
-    settings: RendererSettings,
-    pixiAppOptions: object
-  ): Observable<Renderer> {
-    const subject = new Subject<Renderer>();
-    const renderer = new Renderer(em, settings, pixiAppOptions);
-    renderer.pixiApp.loader.add(sheet).load(() => {
+      }
+      if (errors.length !== 0) {
+        throw Error(`\n${errors.join(`\n`)}`);
+      }
       renderer.resources = renderer.pixiApp.loader.resources[sheet];
-      renderer.pixiApp.ticker.add(() => renderer.renderUpdate());
       subject.next(renderer);
     });
     return subject.pipe(take(1));
   }
 
-  renderUpdate(): void {
-    this.pixiApp.stage = new PIXI.Container();
-
-    const stage = this.pixiApp.stage;
-    const TILE_SIZE = this.settings.tileSize;
-
-    const renderableEntities: Entity[] = [];
-
-    this.maxTileY = -1;
-    this.maxTileX = -1;
-    this.em.each(
-      (e, r, p) => {
-        if (!r.uiElem) {
-          this.maxTileY = Math.max(this.maxTileY, p.y * TILE_SIZE);
-          this.maxTileX = Math.max(this.maxTileX, p.x * TILE_SIZE);
-        }
-        renderableEntities.push(e);
-      },
-      Renderable,
-      GridPos
-    );
-
-    const zValueCalc = (e: Entity) =>
-      e.component(GridPos).z * 100 + e.component(Renderable).zOrder;
-    renderableEntities.sort((lhs, rhs) => zValueCalc(lhs) - zValueCalc(rhs));
-
-    for (const e of renderableEntities) {
-      const r = e.component(Renderable);
-      const p = e.component(GridPos);
-
-      let sprite = this.sprites.get(e.id)!;
-      if (!sprite) {
-        sprite = new PIXI.Sprite(this.resources.textures![r.image]);
-        this.sprites.set(e.id, sprite);
-      }
-
-      sprite.position.set(p.x * TILE_SIZE, this.maxTileY - p.y * TILE_SIZE);
-      stage.addChild(sprite);
+  sprite(name: string): PIXI.Sprite {
+    if (name === '') {
+      throw Error(`Sprite name must not be an empty string`);
     }
-
-    stage.scale.set(this.pixiApp.renderer.width / this.desiredDisplayWidthPx);
+    return new PIXI.Sprite(this.resources.textures![name]);
   }
 
   resize(): void {
     this.pixiApp.resize();
   }
 
-  private convertToGridPos(pos: {
-    x: number;
-    y: number;
-  }): { x: number; y: number } | null {
-    if (this.maxTileX === undefined || this.maxTileY === undefined) {
-      return null;
-    }
-    if (
-      pos.x > this.maxTileX + this.tileSize ||
-      pos.y > this.maxTileY + this.tileSize
-    ) {
-      return null;
-    }
-    const tileX = Math.floor(pos.x / this.tileSize);
-    const tileY = Math.floor(
-      (this.maxTileY + this.tileSize - pos.y) / this.tileSize
-    );
-    return { x: tileX, y: tileY };
+  get view() {
+    return this.pixiApp.view;
   }
+
+  // get mouseOver$() {
+  //   return this.mouseOverGridPos.pipe(
+  //     distinctUntilChanged((lhs, rhs) => {
+  //       if (lhs === rhs) {
+  //         return true;
+  //       } else if (!lhs || !rhs) {
+  //         return false;
+  //       } else {
+  //         return lhs.x === rhs.x && lhs.y === rhs.y;
+  //       }
+  //     })
+  //   );
+  // }
+
+  // get mousePress$() {
+  //   return this.mousePressGridPos;
+  // }
+
+  // private mouseOverGridPos = new Subject<{ x: number; y: number } | null>();
+  // private mousePressGridPos = new Subject<{ x: number; y: number } | null>();
 }
--- rl-ui/src/lib/grid-renderer/grid-renderer.component.ts
+++ rl-ui/src/lib/grid-renderer/grid-renderer.component.ts
@@ -1,7 +1,15 @@
 import { Component, ElementRef, OnInit, ViewChild, Input } from '@angular/core';
 import { Renderer } from '@rad/rad-pixi';
-import { EntityManager } from 'rad-ecs';
+import { EntityManager, Entity } from 'rad-ecs';
 import { ReplaySubject } from 'rxjs';
+import { Renderable } from 'libs/rl-ecs/src/lib/components/renderable.model';
+import { GridPos } from 'libs/rl-ecs/src/lib/components/position.model';
+import * as PIXI from 'pixi.js-legacy';
+
+export interface RendererSettings {
+  tileSize: number;
+  displayWidthInTiles: number;
+}
 
 @Component({
   selector: 'rad-grid-renderer',
@@ -11,26 +19,120 @@ import { ReplaySubject } from 'rxjs';
 export class GridRendererComponent implements OnInit {
   @ViewChild('render', { static: false }) renderElem: ElementRef;
   @Input('em') em: EntityManager;
+  @Input('settings') settings: RendererSettings;
+  @Input('sprites') spriteSheet: string;
+
+  private sprites = new Map<number, PIXI.Sprite>();
+  private desiredDisplayWidthPx: number;
+
+  public maxTileY: number;
+  public maxTileX: number;
 
   private rendererSubject = new ReplaySubject<Renderer>();
+  private renderer: Renderer;
 
-  constructor() {}
+  constructor() {
+    // { tileSize: 16, displayWidthInTiles: 20 }
+    // this.renderer.pixiApp.renderer.plugins.interaction.on('pointermove', event => {
+    //   if (this.renderer.pixiApp.stage) {
+    //     this.mouseOverGridPos.next(
+    //       this.convertToGridPos(event.data.getLocalPosition(this.renderer.pixiApp.stage))
+    //     );
+    //   }
+    // });
+    // this.renderer.pixiApp.renderer.plugins.interaction.on('pointerdown', event => {
+    //   if (this.renderer.pixiApp.stage) {
+    //     this.mousePressGridPos.next(
+    //       this.convertToGridPos(event.data.getLocalPosition(this.renderer.pixiApp.stage))
+    //     );
+    //   }
+    // });
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.desiredDisplayWidthPx =
+      this.settings.tileSize * this.settings.displayWidthInTiles;
+  }
 
   ngAfterViewInit(): void {
-    Renderer.create(
-      this.em,
-      'assets/assets.json',
-      { tileSize: 16, displayWidthInTiles: 20 },
-      {
-        backgroundColor: Number('0x00FFFF'),
-        resizeTo: this.renderElem.nativeElement
-      }
-    ).subscribe((r: Renderer) => {
+    Renderer.create(this.spriteSheet, {
+      backgroundColor: Number('0x00FFFF'),
+      resizeTo: this.renderElem.nativeElement
+    }).subscribe((r: Renderer) => {
       r.resize();
       (this.renderElem.nativeElement as HTMLElement).appendChild(r.view);
       this.rendererSubject.next(r);
+      this.renderer = r;
+      this.renderer.pixiApp.ticker.add(() => this.renderUpdate());
     });
+  }
+
+  renderUpdate(): void {
+    this.renderer.pixiApp.stage = new PIXI.Container();
+
+    const stage = this.renderer.pixiApp.stage;
+    const TILE_SIZE = this.settings.tileSize;
+
+    const renderableEntities: Entity[] = [];
+
+    this.maxTileY = -1;
+    this.maxTileX = -1;
+    this.em.each(
+      (e, r, p) => {
+        if (!r.uiElem) {
+          this.maxTileY = Math.max(this.maxTileY, p.y * TILE_SIZE);
+          this.maxTileX = Math.max(this.maxTileX, p.x * TILE_SIZE);
+        }
+        renderableEntities.push(e);
+      },
+      Renderable,
+      GridPos
+    );
+
+    const zValueCalc = (e: Entity) =>
+      e.component(GridPos).z * 100 + e.component(Renderable).zOrder;
+    renderableEntities.sort((lhs, rhs) => zValueCalc(lhs) - zValueCalc(rhs));
+
+    for (const e of renderableEntities) {
+      const r = e.component(Renderable);
+      const p = e.component(GridPos);
+
+      let sprite = this.sprites.get(e.id)!;
+      if (!sprite) {
+        sprite = this.renderer.sprite(r.image);
+        this.sprites.set(e.id, sprite);
+      }
+
+      sprite.position.set(p.x * TILE_SIZE, this.maxTileY - p.y * TILE_SIZE);
+      stage.addChild(sprite);
+    }
+
+    stage.scale.set(
+      this.renderer.pixiApp.renderer.width / this.desiredDisplayWidthPx
+    );
+  }
+
+  // private convertToGridPos(pos: {
+  //   x: number;
+  //   y: number;
+  // }): { x: number; y: number } | null {
+  //   if (this.maxTileX === undefined || this.maxTileY === undefined) {
+  //     return null;
+  //   }
+  //   if (
+  //     pos.x > this.maxTileX + this.tileSize ||
+  //     pos.y > this.maxTileY + this.tileSize
+  //   ) {
+  //     return null;
+  //   }
+  //   const tileX = Math.floor(pos.x / this.tileSize);
+  //   const tileY = Math.floor(
+  //     (this.maxTileY + this.tileSize - pos.y) / this.tileSize
+  //   );
+  //   return { x: tileX, y: tileY };
+  // }
+
+  get tileSize() {
+    return this.settings.tileSize;
   }
 }
