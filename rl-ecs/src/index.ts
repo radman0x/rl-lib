e298e305da401b46001c38b905e2ef5e105edba8
Conrad Mercer <conrad.mercer@gmail.com>
2020-01-31 15:04:18 +1030

    RAD-480: Expose more types

--- rl-ecs/src/index.ts
+++ rl-ecs/src/index.ts
@@ -1,3 +1,7 @@
+export * from './lib/components/physical.model';
+export * from './lib/components/position.model';
+export * from './lib/components/renderable.model';
+
 export * from './lib/ecs.types';
 export * from './lib/systems/system-organiser.util';
 export * from './lib/systems/systems.utils';
