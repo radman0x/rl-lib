7dd573f5450a4c7d8ac8bc4379c369ef6bb1df06
Conrad Mercer <conrad.mercer@gmail.com>
2021-03-11 16:32:05 +1030

    RAD-554: Health displayed as coloured bar

--- rl-ui/src/lib/stats-display/stats-display.component.ts
+++ rl-ui/src/lib/stats-display/stats-display.component.ts
@@ -3,15 +3,21 @@ import {
   Armor,
   Attacks,
   getModifiedComponent,
-  Martial,
   recursiveObserveEntity,
   Wounds,
 } from '@rad/rl-ecs';
-import { Damage } from 'libs/rl-ecs/src/lib/components/damage.model';
 import { Strength } from 'libs/rl-ecs/src/lib/components/strength.model';
 import { Toughness } from 'libs/rl-ecs/src/lib/components/toughness.model';
 import { WeaponSkill } from 'libs/rl-ecs/src/lib/components/weapon-skill.model';
 import { EntityId, EntityManager } from 'rad-ecs';
+
+enum PercentColors {
+  VERY_HIGH = `#11970E`,
+  HIGH = `#FFFF00`,
+  MODERATE = `#D17700`,
+  LOW = `#FF5733`,
+  VERY_LOW = `#C70039`,
+}
 
 @Component({
   selector: 'rad-stats-display',
@@ -29,8 +35,9 @@ export class StatsDisplayComponent implements OnInit {
     { desc: 'DAMAGE', value: '-' },
     { desc: 'ARMOR', value: '-' },
     { desc: 'WARD', value: '-' },
-    { desc: 'WOUNDS', value: '-' },
   ];
+
+  public wounds: Wounds | null = null;
 
   private WS_INDEX = 0;
   private S_INDEX = 1;
@@ -38,7 +45,8 @@ export class StatsDisplayComponent implements OnInit {
   private D_INDEX = 3;
   private ARMOR_INDEX = 4;
   private WARD_INDEX = 5;
-  private W_INDEX = 6;
+
+  public woundDisplayWidth = 200;
 
   constructor(private changeDetector: ChangeDetectorRef) {}
 
@@ -64,10 +72,48 @@ export class StatsDisplayComponent implements OnInit {
     this.updateValue(Strength, this.S_INDEX, (s) => s.count.toString());
     this.updateValue(Toughness, this.T_INDEX, (t) => t.count.toString());
     this.updateValue(Attacks, this.D_INDEX, (a) => a.damage.toString());
-    this.updateValue(Wounds, this.W_INDEX, (w) => w.current.toString());
     this.updateValue(Armor, this.ARMOR_INDEX, (arm) => arm.count.toString());
+    this.updateWounds();
 
     this.changeDetector.detectChanges();
+  }
+
+  public woundsStyle() {
+    const woundPercent = this.wounds.current / this.wounds.max;
+    let color = '';
+    if (woundPercent >= 0.8) {
+      color = PercentColors.VERY_HIGH;
+    } else if (woundPercent >= 0.6) {
+      color = PercentColors.HIGH;
+    } else if (woundPercent >= 0.4) {
+      color = PercentColors.MODERATE;
+    } else if (woundPercent >= 0.2) {
+      color = PercentColors.LOW;
+    } else {
+      color = PercentColors.VERY_LOW;
+    }
+
+    return {
+      width: `${this.woundDisplayWidth * woundPercent}px`,
+      backgroundColor: color,
+      fontWeight: 'bold',
+    };
+  }
+
+  public woundText() {
+    const woundPercent = this.wounds.current / this.wounds.max;
+    let color = `#FFFFFF`;
+    if (woundPercent >= 0.6) {
+      color = `#888888`;
+    }
+
+    return {
+      color,
+    };
+  }
+
+  private updateWounds() {
+    this.wounds = getModifiedComponent(this.statsEntityId, Wounds, this.em);
   }
 
   private updateValue<
